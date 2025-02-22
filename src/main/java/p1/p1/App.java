0166c2af7004249419a19cdca83b9eac8459f6ce
Yousif Albayati <yalbayat@my.centennialcollege.ca>
2025-02-21 22:33:26 -0500

    Initial commit

---
+++ src/main/java/p1/p1/App.java
@@ -0,0 +1,13 @@
+package p1.p1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to COMP367" );
+    }
+}
